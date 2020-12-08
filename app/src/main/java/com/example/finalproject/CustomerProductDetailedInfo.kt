caa8a669dcd6f43a1869065eac3adf65de49c851
DaMish96 <52632514+DaMish96@users.noreply.github.com>
2020-12-08 03:37:04 -0500

    CustomerProductDetailedInfo + minor changes

---
+++ app/src/main/java/com/example/finalproject/CustomerProductDetailedInfo.kt
@@ -0,0 +1,11 @@
+package com.example.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerProductDetailedInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_product_detailed_info)
+    }
+}
