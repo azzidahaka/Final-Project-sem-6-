d3499d846edc568acc2135fb4eb0cb15903899fc
DaMish96 <52632514+DaMish96@users.noreply.github.com>
2020-12-09 03:54:53 -0500

    implement scanner(half way)

--- app/src/main/java/com/example/finalproject/CustomerHome.kt
+++ app/src/main/java/com/example/finalproject/CustomerHome.kt
@@ -1,16 +1,27 @@
 package com.example.finalproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class CustomerHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_home)
     }
+//
+//    lateinit var mCamera: Camera
+//
+//    var options = BarcodeScannerOptions.Builder()
+//            .setBarcodeFormats(
+//                    Barcode.FORMAT_EAN_13,
+//                    Barcode.FORMAT_EAN_8,
+//                    Barcode.FORMAT_CODE_93
+//            ).build()
+//
 
     fun onScannerClick(view: View) {
-
+        //call another activity
     }
 }
